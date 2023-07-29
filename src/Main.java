0efeb6a6a49b4139e4346c45318d944390bba368
MGPKrohn <matheuskrohn@gmail.com>
2023-07-29 20:47:58 -0300

    Correção Nome e Endereço Composto

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<Pessoa> pessoas = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
 
         String mensagem = "Cadastro de pessoas e endereço";
         String linha = "====================================";
@@ -20,7 +25,7 @@ public class Main {
         // Cadastrar pessoas com n endereços
         for (int i = 0; i < qtdPessoas; i++) {
             System.out.println("Digite o nome: ");
-            String nome = scanner.next();
+            String nome = reader.readLine();
             System.out.println("Digite a idade: ");
             int idade = scanner.nextInt();
             System.out.println("Digite a quantidade de endereços: ");
@@ -29,7 +34,7 @@ public class Main {
             List<Endereco> enderecos = new ArrayList<>();
             for (int j = 0; j < qtdEndereco; j++) {
                 System.out.println("Digite o endereço:");
-                String rua = scanner.next();
+                String rua = reader.readLine();
                 System.out.println("Digite o número do endereço:");
                 int numero = scanner.nextInt();
                 enderecos.add(new Endereco(rua, numero));
@@ -43,12 +48,13 @@ public class Main {
             System.out.println("Nome: " + pessoa.getNome());
             System.out.println("Idade: " + pessoa.getIdade());
             List<Endereco> enderecos = pessoa.getEnderecos();
-            System.out.println("Endereços:");
+            System.out.print("Endereço:");
             for (Endereco endereco : enderecos) {
-                System.out.println(endereco.getRua() + ", " + endereco.getNumero());
+                System.out.print(endereco.getRua() + ", " + endereco.getNumero());
             }
-            System.out.println();
         }
+
+        System.out.println();
 
         // Buscar uma pessoa pelo nome e imprimir os dados na tela
         System.out.println("Digite o nome da pessoa que deseja buscar: ");
@@ -58,9 +64,9 @@ public class Main {
                 System.out.println("Nome: " + pessoa.getNome());
                 System.out.println("Idade: " + pessoa.getIdade());
                 List<Endereco> enderecos = pessoa.getEnderecos();
-                System.out.println("Endereços:");
+                System.out.print("Endereço:");
                 for (Endereco endereco : enderecos) {
-                    System.out.println(endereco.getRua() + ", " + endereco.getNumero());
+                    System.out.print(endereco.getRua() + ", " + endereco.getNumero());
                 }
                 return;
             }
